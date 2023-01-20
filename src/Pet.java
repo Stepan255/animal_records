b194d11fbd54557b4e7daa255f8ff6ca2f43c0c0
Stepan <St3Ba4@gmail.com>
2023-01-20 11:32:44 +0400

    Create class with method encapsulation and explore by diagram

---
+++ src/Pet.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Pet extends Animal{
+    public Pet(String name, Date birthday, ArrayList<String> commands) {
+        super(name, birthday, commands);
+    }
+}
